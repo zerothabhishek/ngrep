036b238119e1adcbd8cf9f7d7b90154200aef87c
abhishek <bigbeliever@gmail.com>
2013-06-01 16:59:31 +0530

    added pre/post options to find. moved logic from thor script to search code

--- lib/ng.thor
+++ lib/ng.thor
@@ -1,24 +1,36 @@
 ##!/usr/bin/env ruby
 require "thor"
 
+
+## After changes to this file, run: thor update ng.thor
+
 class Ng < Thor
 
 	desc "find TERM", "search the given term"
+	option :pre, :type => :numeric, :aliases => :a
+	option :post, :type => :numeric, :aliases => :b 
 	def find(term)
-		require "/Users/abhishekyadav/code/ngrep-proj/ngrep/lib/search.rb"
-		search = Ngrep::Search.new
-		results = search.run(term)
-		results.each do |result|
-			puts result["project"].to_s + ": " + result["date"] 
-		end
+		require '/Users/abhishekyadav/code/ngrep-proj/ngrep/ngrep.rb'
+		puts Ngrep::Search.find(term, options)
 	end
 
 	desc "rehash", "rebuild the index"
 	def rehash
-		puts "rebuilding the index"
+		Ngrep::Build.new(filename: "/path/to/file").run
+	end
+
+	desc "add FILE", "adds file to watch list"
+	def add(file)
+		puts "adding #{file}"
+	end
+
+	desc "remove FILE", "removes file from watch list"
+	def remove(file)
+		puts "removing #{file}"
 	end
 
 	#default_task :search ## doesn't work right
 end
 
-#Ng.start
+#Ng.start  # uncomment to make this a shell command
+
--- lib/search.rb
+++ lib/search.rb
@@ -3,9 +3,7 @@
 module Ngrep
 	class Search
 
-		#DataFile = "/Users/abhishekyadav/code/ngrep-proj/ngrep/data/hash.json"
 		def data_file
-			#"/Users/abhishekyadav/code/ngrep-proj/ngrep/data/hash.json"
 			Ngrep::Config[:data_file]
 		end
 
@@ -20,6 +18,59 @@ module Ngrep
 
 		def load_index
 			@index = JSON.parse File.read data_file
+		end
+
+		def find(term, options)
+			pre = options[:pre].to_i
+			post = options[:post].to_i
+			results = self.run(term) || []
+			results.map{|r| result_string(r, pre, post) }.join("\n")
+		end
+
+		def result_string(r, pre, post)
+			str = ""
+			str += "*** "                ## each result starts with this symbol
+			str += result_project(r)     ## first the project name
+			str += ", "                  ## comma after the project name
+			str += result_date(r)        ## then the result date
+			str += ". "                  ## comma after result date
+			str += result_line_no(r)                      ## then the line no. 
+			str += pre==0 && post==0 ? ": " : ": \n"      ## line break if pre/post lines are needed
+			str += result_lines(r, pre, post)             ## and the result lines, with pre/post lines
+			str                                   
+		end
+
+		def result_project(result)
+			result["project"].to_s
+		end
+
+		def result_date(result)
+			result["date"].to_s
+		end
+
+		def result_line_no(result)
+			result["line_number"].to_s
+		end
+
+		def result_lines(result, pre=0, post=0)
+			file_path = result["file_path"]
+			line_no   = result["line_number"]
+			lines_from_file(file_path, line_no, pre, post).map do |line|
+				line.gsub("\n", '')  ## remove the line-break at the end
+			end.join("\n")
+		end
+
+		def lines_from_file(file_path, line_no, pre, post)
+			f = File.open(file_path, "r")
+			(line_no-pre).times{ f.gets }
+			num = pre + post + 1
+			lines = (1..num).map { f.gets }
+			f.close
+			lines
+		end
+
+		def self.find(term, options={})
+			Ngrep::Search.new.find(term, options)
 		end
 
 	end	
