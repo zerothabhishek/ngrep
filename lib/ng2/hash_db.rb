ebcb27dc4a030c6598c3f106258411e2b6672d8e
abhishek <bigbeliever@gmail.com>
2013-10-28 15:42:40 +0530

    Major changes to Ng2::HashDb api and operation

--- lib/ng2/hash_db.rb
+++ lib/ng2/hash_db.rb
@@ -2,48 +2,118 @@ require 'kyotocabinet'
 
 class Ng2::HashDb
 
-	attr_accessor :db
+	@@db_scanner  = nil
+	@@db_searcher = nil
+	@@db_file     = nil
+
+	## Getters and setters
+	def self.db_scanner
+		@@db_scanner
+	end
+
+	def self.db_scanner=x
+		@@db_scanner=x
+	end
+
+	def self.db_file
+		@@db_file
+	end
+
+	def self.db_file=x
+		@@db_file=x
+	end
+
+	def self.db_searcher
+		@@db_searcher
+	end
 
 	def self.default_db_path
-		File.expand_path("~/.ng2/hash.kc")
+		ENV && ENV=='test' ?
+			File.expand_path('../../../tmp/hash.kch', __FILE__) :
+			File.expand_path("~/.ng2/hash.kch")
 	end
 
-	def initialize(db_path=nil)
-		@db_file = db_path || self.default_db_path
-		@db = KyotoCabinet::DB.new
+	## Main api
+	def self.add(key, given_value)
+		existing_value = self.get(key)
+		value = existing_value.size==0 ? given_value :
+						concat(existing_value, given_value)
+		self.set(key, value)
 	end
 
-	def set(key, value)
-		@db.open(@db_file, KyotoCabinet::DB::OWRITER | KyotoCabinet::DB::OCREATE)
-		@db.set(key, value)
-		@db.close
-	end
-
-	def get(key)
-		@db.open(@db_file, KyotoCabinet::DB::OREADER)
-		value = @db.get(key)
-		@db.close
-		value
-	end
-
-	def self.add(key, value)
-		hash_db = self.new
-		existing_value = hash_db.get(key)
-		if existing_value
-			value = existing_value + "|" + value
-		end
-		hash_db.set(key, value)
+	def self.set(key, value)
+		raise "HashDb.setup first" unless @@db_scanner
+		@@db_scanner.set(key, value) or raise "Can't set on the KC db"
 	end
 
 	def self.get(key)
-		hash_db = self.new
-		value_str = hash_db.get(key)
-		if value_str
-			value_str.split("|")
-		else
-			[]
-		end
+		raise "HashDb.setup first" if @@db_scanner.nil? && @@db_searcher.nil?
+		db_reader = @@db_scanner || @@db_searcher
+		value_str = db_reader.get(key)
+		value_str ? value_str.split("|") : []
 	end
 
+	def self.clear
+		@@db_scanner.clear
+	end
+
+	def self.concat(existing_values, new_value)
+		arr = existing_values.push new_value.to_s
+		arr.flatten
+		arr.join("|")
+	end
+
+	## Scanner
+	def self.setup
+		@@db_file = self.default_db_path
+		open_scanner
+	end
+
+	def self.teardown
+		@@db_scanner.close
+		@@db_scanner = nil
+	end
+
+	def self.open_scanner
+		@@db_scanner = KyotoCabinet::DB.new
+		@@db_scanner.open(@@db_file.to_s,         # to_s because it doesn't fail when @@db_file is nil
+			KyotoCabinet::DB::OWRITER |
+			KyotoCabinet::DB::OCREATE)    or raise "Can't open the KC db"
+	end
+
+	# db_scanner  [not used anywhere]
+	# - used in rehashing and watch_handling
+	# - used in testing, as value verification can't be done w/o reopening the db
+	# - can do both read and write
+	# - will be blocked if there is another thread with a searcher
+	# - will block other read-write threads
+	def self.with_scanner
+		setup
+		yield
+		teardown
+	end
+
+	# searcher
+	def self.setup_searcher
+		@@db_file = self.default_db_path
+	  @@db_searcher = KyotoCabinet::DB.new
+	  @@db_searcher.open(@@db_file, KyotoCabinet::DB::OREADER) or raise "Can't open the KC db"
+	end
+
+	def self.teardown_searcher
+	  @@db_searcher.close
+	  @@db_searcher = nil
+	end
+
+	# db_searcher  [not used anywhere]
+	# - used only for reading (get)
+	# - can't get latest values: those that the same thread may be writing
+	# - can be used for threaded read
+	# - will be blocked if a scanner runs in another thread
+	def self.with_searcher
+		setup_searcher
+		yield
+		teardown_searcher
+	end
 
 end
--- test/hash_db_test.rb
+++ test/hash_db_test.rb
@@ -1,52 +1,173 @@
 require File.expand_path '../test_helper', __FILE__
 
-class HashDbTest2 < MiniTest::Test
+class HashDbTest < MiniTest::Test
 
-	include DbHelpers
+	#include DbHelpers
 
 	def setup
+		set_scanner
 		empty_test_db
-		@key = "sample-key"
-		@value = "sample-value"
-		@existing_value = "existing-value"
 	end
 
-	def test_1
-		desc = "#set: should set the given key value"
-		Ng2::HashDb.new(test_db_path).set(@key, @value)
-		assert_equal @value, db_get(@key), desc
+	def teardown
+		close_scanner
 	end
 
-	def test_2
-		desc = "#get: should get the asked key value"
-		db_set(@key, @value)
-		assert_equal @value, Ng2::HashDb.new(test_db_path).get(@key), desc
+	def test_add
+		desc = '.add should add a given value to the db'
+
+		Ng2::HashDb.add("sample-key", "sample-value")
+		assert_equal "sample-value", db_lookup("sample-key"), desc
 	end
 
-	def test_3
-		desc = ".add: should append the new value if key pre-exists"
-		db_set(@key, @existing_value)
-		Ng2::HashDb.add(@key, @value)
-		expected_value = "#{@existing_value}|#{@value}"
-		assert_equal expected_value, db_get(@key), desc
+	def test_add_1
+		desc = '.add should append if the value already exists'
+
+		Ng2::HashDb.add('sample-key', 'existing-value')
+		Ng2::HashDb.add('sample-key', 'sample-value')
+		assert_equal 'existing-value|sample-value', db_lookup('sample-key'), desc
 	end
 
-	def test_4
-		desc = ".add: should just the new value if key doesn't pre-exist"
-		Ng2::HashDb.add(@key, @value)
-		assert_equal @value, db_get(@key), desc
+	def test_get
+		desc = '.get gets a list of values at the key (single-value)'
+
+		Ng2::HashDb.add('sample-key', 'sample-value')
+		assert_equal ['sample-value'], Ng2::HashDb.get('sample-key'), desc
 	end
 
-	def test_5
-		desc = ".get: gives a list of values at a key"
-		db_set(@key, 'v1|v2|v3')
-		expected_value = ['v1', 'v2', 'v3']
-		assert_equal expected_value, Ng2::HashDb.get(@key), desc
+	def test_get_1
+		desc = '.get gets a list of values at the key (two values)'
+
+		Ng2::HashDb.add('sample-key', 'existing-value')
+		Ng2::HashDb.add('sample-key', 'sample-value')
+		assert_equal ['existing-value', 'sample-value'], Ng2::HashDb.get('sample-key'), desc
 	end
 
-	def test_6
-		desc = ".get: gives an empty array if key doesn't exist"
-		assert_equal [], Ng2::HashDb.get(@key), desc
+	def test_get_2
+		desc = '.get gets a list of values at the key (no-value)'
+		assert_equal [], Ng2::HashDb.get('sample-key'), desc
+	end
+
+	def test_set
+		desc = '.set sets the given value to the db'
+
+		Ng2::HashDb.set('sample-key', 'sample-value')
+		assert_equal 'sample-value', db_lookup('sample-key'), desc
+	end
+
+	def test_set_1
+		desc = '.set overwrites if the value exists'
+
+		Ng2::HashDb.set('sample-key', 'existing-value')
+		Ng2::HashDb.set('sample-key', 'sample-value')
+		assert_equal ['sample-value'], Ng2::HashDb.get('sample-key'), desc
+	end
+
+	def test_clear
+		desc = '.clear deletes everything from the db'
+
+		Ng2::HashDb.clear
+		h={}; @scanner.each_pair{|k,v| h[k]=v}
+		assert h.empty?, desc
+	end
+
+	def test_concat
+		desc = ".concat adds the new value to the existing-values list"
+
+		existing_values = ['existing-value']
+		result = Ng2::HashDb.concat(existing_values, 'new-value')
+		assert_equal 'existing-value|new-value', result
+	end
+
+	# Helpers
+	def set_scanner
+		db_file = File.expand_path('../../tmp/hash.kch', __FILE__)
+		@scanner = KyotoCabinet::DB.new
+		@scanner.open(db_file, KyotoCabinet::DB::OWRITER | KyotoCabinet::DB::OCREATE)
+		Ng2::HashDb.db_scanner=@scanner
+	end
+
+	def close_scanner
+		@scanner.close
+	end
+
+	def empty_test_db
+		@scanner.clear
+	end
+
+	def db_lookup(key)
+		@scanner.get(key)
+	end
+
+end
+
+
+# separate class for tests where setup/teardown rules are different
+class HashDbTest2 < MiniTest::Test  
+
+	def test_open_scanner
+		desc = ".open_scanner opens the db-file in write/create mode"
+
+		Ng2::HashDb.db_file = "/tmp/sample.kch"
+		Ng2::HashDb.open_scanner
+		assert Ng2::HashDb.db_scanner, desc
+	end
+
+	def test_open_scanner_2
+		desc = ".open_scanner throws exception if there's an error in opening the db"
+
+		begin 
+			Ng2::HashDb.db_file = nil
+			Ng2::HashDb.open_scanner
+		rescue RuntimeError => e
+			assert_equal "Can't open the KC db", e.message, desc
+		end
+	end
+
+	def test_setup
+		desc = ".setup sets the db_file"
+
+		Ng2::HashDb.setup
+		assert Ng2::HashDb.db_file, desc
+	end
+
+	def test_setup_2
+		desc = ".setup opens the scanner"
+		Ng2::HashDb.setup
+		assert Ng2::HashDb.db_scanner, desc
+	end
+
+	def test_teardown
+		desc = ".teardown closes the open scanner"
+		Ng2::HashDb.setup
+		Ng2::HashDb.teardown
+		assert_equal nil, Ng2::HashDb.db_scanner, desc
+	end
+
+	def test_setup_searcher
+		desc = ".setup_searcher opens the db in read mode"
+
+		Ng2::HashDb.db_file = "/tmp/sample.kch"
+		Ng2::HashDb.setup_searcher
+		assert Ng2::HashDb.db_searcher, desc
+	end
+
+	def test_setup_seacher_2
+		desc = ".setup_searcher throws exception if opening the db fails"
+
+		begin
+			Ng2::HashDb.db_file = nil
+			Ng2::HashDb.setup_searcher
+		rescue RuntimeError => e
+			assert_equal "Can't open the KC db", e.message, desc
+		end
+	end
+
+	def test_teardown_searcher
+		desc = ".teardown_searcher closes the open searcher"
+		Ng2::HashDb.setup_searcher
+		Ng2::HashDb.teardown_searcher
+		assert_equal nil, Ng2::HashDb.db_searcher, desc
 	end
 
 end
